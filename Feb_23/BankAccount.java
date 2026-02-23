e0e9169f496a1655209e2892aeef5343c2766a45
Govind9066 <gadekargovind575@gmail.com>
2026-02-23 13:16:11 +0530

    emp program with constructor

---
+++ Feb_23/BankAccount.java
@@ -0,0 +1,15 @@
+package Feb_23;
+
+class Bank{
+    int acNo;
+    String acHolder;
+    int bal;
+
+    
+}
+
+public class BankAccount {
+    public static void main(String[] args) {
+
+    }
+}
--- Feb_23/EmployeeDetails.java
+++ Feb_23/EmployeeDetails.java
@@ -1,4 +1,37 @@
 package Feb_23;
 
+class emp{
+    String name;
+    int id;
+    int sal;
+
+    public emp(String name, int id, int sal) {
+        this.name = name;
+        this.id = id;
+        this.sal = sal;
+    }
+
+    void displayempdetails(){
+        System.out.println("---------------------");
+        System.out.println("Employee details :");
+        System.out.println("Name:"+name);
+        System.out.println("Id:"+id);
+        System.out.println("Salary:"+sal);
+    }
+
+    void bonus(){
+        System.out.println("----------------------");
+        int parcent=(sal/10)*12;
+        System.out.println("Yearly Bonuse is  : ");
+        System.out.println(parcent);
+    }
+}
+
 public class EmployeeDetails {
+    public static void main(String[] args) {
+        emp e1=new emp("Krishna",102,500);
+        e1.displayempdetails();
+        e1.bonus();
+        e1.displayempdetails();
+    }
 }
