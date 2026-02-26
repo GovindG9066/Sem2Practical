f9b4bdcd8dab640d1d33ce58484f48855a968cd4
Govind9066 <gadekargovind575@gmail.com>
2026-02-26 16:17:54 +0530

    Product Market program compleated with some methods like display,updatestock and calculatetotalPrice of the product

--- Feb_23/ProductMarket.java
+++ Feb_23/ProductMarket.java
@@ -1,54 +1,65 @@
-//package Feb_23;
-//
-//import java.util.Scanner;
-//
-//class product{
-//    String name;
-//    int id, price,quentity;
-//    public product(String name,int id,int price,int quentity){
-//        this.name=name;
-//        this.id=id;
-//        this.price=price;
-//        this.quentity=quentity;
-//    }
-//    void display(){
-//        System.out.println("Product Details:");
-//        System.out.println("Name of the product :"+name);
-//        System.out.println("Id of the product :"+id);
-//        System.out.println("Price of the product :"+price);
-//        System.out.println("Quentity of the product :"+quentity);
-//    }
-//    class invalidinput extends Exception{
-//        invalidinput(String msg){
-//            super(msg);
-//        }
-//
-//    }
-//    void updateStock(int addreduce,int newQuentity){
-//        try {
-//            if(addreduce==1){
-//
-//                if(addreduce>quentity) {
-//                    throw new invalidinput("input out of bount");
-//                }else{
-//                    System.out.println("Reduce quentity is :"+newQuentity);
-//                    quentity-=newQuentity;
-//                }
-//            }
-//            } else if (addreduce==2) {
-//                System.out.println("Add quentity is :"+newQuentity);
-//            }else {
-//                throw new invalidinput("Enter valid input");
-//            }
-//        }catch (invalidinput e){
-//            System.out.println(e.getMessage());
-//        }
-//    }
-//}
-//
-//public class ProductMarket {
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//
-//    }
-//}
+package Feb_23;
+import java.util.Scanner;
+class Product{
+    String name;
+    int id;
+    double price;
+    int quentity;
+
+    public Product(String name, int id, double price, int quentity) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.quentity = quentity;
+    }
+
+    void display(){
+        System.out.println("Display All Products :");
+        System.out.println("Name :"+name);
+        System.out.println("Id :"+id);
+        System.out.println("Price :"+price);
+        System.out.println("Quentity :"+quentity);
+    }
+
+    void UpdateStock(int choice,int newStock){
+
+        if(choice==1){
+            quentity+=newStock;
+        } else if (choice==2) {
+            quentity-=newStock;
+        }else {
+            System.out.println("Not Correct choice please try again");
+        }
+    }
+    void CalculateTotal(){
+        System.out.println("Total Value of product :"+(quentity*price));
+    }
+}
+
+public class ProductMarket {
+    public static void main(String[] args) {
+        Product p1=new Product("Phone",125,8000,12);
+        p1.display();
+        System.out.println("--------------------------------");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Your Choice :");
+        System.out.println("Press 1 for add new Stock");
+        System.out.println("Press 2 for remove the Stock");
+        System.out.println("--------------------------------");
+
+        System.out.println("Enter the Choice :");
+        int choice=sc.nextInt();
+        System.out.println("Enter New Stock :");
+        int newStock=sc.nextInt();
+        p1.UpdateStock(choice,newStock);
+        System.out.println("--------------------------------");
+
+        p1.display();
+        System.out.println("--------------------------------");
+
+        p1.CalculateTotal();
+        System.out.println("--------------------------------");
+
+
+    }
+}
