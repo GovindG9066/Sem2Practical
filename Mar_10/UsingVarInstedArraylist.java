b6eeb16c7419f93bc075ef69596875c0baf85bcf
Govind9066 <gadekargovind575@gmail.com>
2026-03-10 15:45:47 +0530

    use the var keyword insted of ArrayList keyword

--- Mar_10/UsingVarInstedArraylist.java
+++ Mar_10/UsingVarInstedArraylist.java
@@ -1,4 +1,14 @@
 package Mar_10;
 
+import java.util.ArrayList;
+
 public class UsingVarInstedArraylist {
+    public static void main(String[] args) {
+        var cars=new ArrayList<String>();
+        cars.add("BMW");
+        cars.add("Farari");
+        cars.add("Mastang gt");
+
+        cars.forEach(car-> System.out.println(car));
+    }
 }
